1ccea10772be10416e7c581799dbcdc870ade617
Markus Paasiniemi <madp-git-7117@2pi.fi>
2025-10-01 07:50:57 +0300

    test(15min_prices): add test that get_prices works for 15minute interval prices

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,6 +8,9 @@ from entsoe_cli import get_prices
 
 FI = ZoneInfo("Europe/Helsinki")
 
+HOUR_SECONDS = 3600
+QUARTER_SECONDS = 900
+
 
 def test_invalid_range():
     """Invalid range results in an error"""
@@ -49,8 +52,26 @@ def test_get_prices_long_period():
     end_time = datetime(2024, 6, 20, tzinfo=FI)
     prices = list(get_prices(start_time, end_time))
     # expected number or prices
-    len_exp = (end_time.timestamp() - start_time.timestamp()) // 3600
+    len_exp = (end_time.timestamp() - start_time.timestamp()) // HOUR_SECONDS
     assert len(prices) == len_exp
+    # no duplicates
+    assert len(prices) == len({p.start_time for p in prices})
+    # in order
+    assert [p.start_time for p in prices] == sorted(p.start_time for p in prices)
+
+
+def test_quarterly_prices():
+    """get_prices works for a quarterly prices, even if the frequqnce changes"""
+    start_time = datetime(2025, 10, 1, tzinfo=FI)
+    end_time = datetime(2025, 10, 2, tzinfo=FI)
+    prices = list(get_prices(start_time, end_time))
+    # first price is hourly, next are quarterly
+    assert (prices[1].start_time - prices[0].start_time).total_seconds() == HOUR_SECONDS
+    for i in range(1, len(prices) - 1):
+        diff = prices[i + 1].start_time - prices[i].start_time
+        assert diff.total_seconds() == QUARTER_SECONDS
+    # last price is for the last quarter
+    assert (end_time - prices[-1].start_time).total_seconds() == QUARTER_SECONDS
     # no duplicates
     assert len(prices) == len({p.start_time for p in prices})
     # in order
